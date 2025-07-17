083fc332f734a53ac6fe722f65d2145d5f732696
pengppng <pannaporn.boo@gmail.com>
2025-07-17 14:07:54 +0700

    for aws and variables.tf for manage next step

--- learn-terraform-get-started-aws/main.tf
+++ learn-terraform-get-started-aws/main.tf
@@ -1,21 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "us-west-2"
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name = "cloudmigrate"
+    name = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
@@ -24,7 +15,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "learn-terraform"
