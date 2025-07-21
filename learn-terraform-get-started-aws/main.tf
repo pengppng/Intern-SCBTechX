1a474f5ec20675e2961e7a1cdd07e2672cec53a2
pengppng <pannaporn.boo@gmail.com>
2025-07-21 09:01:15 +0700

    public and output out

--- learn-terraform-get-started-aws/main.tf
+++ learn-terraform-get-started-aws/main.tf
@@ -63,6 +63,8 @@ resource "aws_instance" "app_server" {
   instance_type          = var.instance_type
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+  
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
