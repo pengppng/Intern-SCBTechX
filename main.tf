82a8e273ab9ddf6b8165d31796721fa8176ab074
pengppng <pannaporn.boo@gmail.com>
2025-07-24 11:42:39 +0700

    add output

--- main.tf
+++ main.tf
@@ -85,3 +85,15 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.preecr.repository_url
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
