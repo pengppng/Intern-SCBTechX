0e4078500150cabb5bd330d5a3bed73cac361e75
pengppng <pannaporn.boo@gmail.com>
2025-07-24 14:58:13 +0700

    eks

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,108 +11,52 @@ terraform {
     }
   }
 }
-
 provider "aws" {
-  region = "ap-southeast-1"
+  region = var.aws_region
 }
-
-variable "repository_name" {
-  default = "preecr"
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
 }
-
-variable "lifecycle_policy" {
-  default = "lifecycle-policy.json"
-}
-
-resource "aws_ecr_repository" "preecr" {
-  name                 = var.repository_name
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  encryption_configuration {
-    encryption_type = "KMS"
-  }
-
-  tags = {
-    Name        = var.repository_name
-    Environment = "dev"
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "preecr_policy" {
-  repository = aws_ecr_repository.preecr.name
-  policy     = file(var.lifecycle_policy)
-}
-
-# ------------------------------------
-# ✅ เพิ่ม VPC สำหรับ EKS
-# ------------------------------------
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.1"
-
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Environment = "dev"
-  }
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "4.0.2"
+  name           = "poc-vpc"
+  cidr           = "10.0.0.0/16"
+  azs            = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets = ["10.0.10.0/24", "10.0.20.0/24"]
+  map_public_ip_on_launch = true
+  enable_nat_gateway   = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
-
-# ------------------------------------
-# ✅ เพิ่ม EKS Cluster
-# ------------------------------------
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.15"
-
-  cluster_name    = "preecr-cluster"
+  version = "~> 20.0"
+  cluster_name    = "poc-eks"
   cluster_version = "1.27"
-
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
+  subnet_ids = module.vpc.public_subnets
   enable_cluster_creator_admin_permissions = true
-
   eks_managed_node_group_defaults = {
     instance_types = ["t3.small"]
     disk_size      = 20
     capacity_type  = "ON_DEMAND"
   }
-
   eks_managed_node_groups = {
-    preecr = {
+    poc_nodes = {
       desired_size = 1
       min_size     = 1
-      max_size     = 2
+      max_size     = 1
     }
   }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
 }
-
-
-output "ecr_repo_url" {
-  value = aws_ecr_repository.preecr.repository_url
+output "cluster_name" {
+  value = module.eks.cluster_id
 }
-
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "eks_cluster_endpoint" {
+output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
+}
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
 }
