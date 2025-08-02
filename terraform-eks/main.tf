68a58db72328bef8f6c07591e713980e124bf1fd
pengppng <pannaporn.boo@gmail.com>
2025-08-02 17:18:54 +0700

    try to edit pls work

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "cicd-terraform-eks"
+    key    = "eks/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
 }
 
 provider "aws" {
@@ -18,22 +24,53 @@ variable "aws_region" {
   default = "ap-southeast-1"
 }
 
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "Private Subnets CIDR"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "Public Subnets CIDR"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+data "aws_availability_zones" "azs" {}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.2"
 
   name                 = "eks-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.11.0/24", "10.0.12.0/24"]
+  cidr                 = var.vpc_cidr
+  azs                  = data.aws_availability_zones.azs.names
+  private_subnets      = var.private_subnets
+  public_subnets       = var.public_subnets
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Environment = "dev"
+    Environment                             = "dev"
+    "kubernetes.io/cluster/preecr-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/preecr-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/preecr-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = 1
   }
 }
 
@@ -41,15 +78,15 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.2"
 
-  cluster_name    = "preecr-cluster"
-  cluster_version = "1.33"
+  cluster_name                    = "preecr-cluster"
+  cluster_version                 = "1.28"
+  cluster_endpoint_public_access  = true
+  enable_irsa                     = true
+  manage_aws_auth                 = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-  cluster_endpoint_public_access  = true
 
-  enable_irsa        = true
-  manage_aws_auth    = true
   aws_auth_users = [
     {
       userarn  = "arn:aws:iam::123456789012:user/admintest"
@@ -57,9 +94,6 @@ module "eks" {
       groups   = ["system:masters"]
     }
   ]
-
-  create_kms_key              = false
-  cluster_encryption_config   = []
 
   eks_managed_node_group_defaults = {
     instance_types = ["t3.small"]
@@ -75,21 +109,23 @@ module "eks" {
     }
   }
 
+  create_kms_key            = false
+  cluster_encryption_config = []
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
 }
 
-
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  value = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
-  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+  value = module.eks.cluster_security_group_id
 }
