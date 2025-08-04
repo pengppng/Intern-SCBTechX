45c6ff633020fdb624ec4050ab9b9a35d736496e
pengppng <pannaporn.boo@gmail.com>
2025-08-04 13:25:00 +0700

    Remove unused VPC module references in main.tf

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -42,9 +42,6 @@ module "vpc" {
   }
 }
 
-vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
