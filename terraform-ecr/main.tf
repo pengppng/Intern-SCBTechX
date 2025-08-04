32b75147667e5d538d992ff66adc5f2a879032fd
pengppng <pannaporn.boo@gmail.com>
2025-08-04 10:25:07 +0700

    Refactor Jenkinsfile and Terraform configuration for ECR setup

--- terraform-ecr/main.tf
+++ terraform-ecr/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "png-terraform-state-bucket"
+    key            = "ecr/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
