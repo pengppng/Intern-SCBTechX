3511065665921daac95d84f807ab51a6c8664b67
pengppng <pannaporn.boo@gmail.com>
2025-08-04 11:09:35 +0700

    Add backend configuration to Terraform and implement Jenkins pipeline for EKS deployment

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -1,11 +1,16 @@
 terraform {
   required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+  backend "s3" {
+    bucket         = "png-terraform-state-bucket"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
   }
 }
 
